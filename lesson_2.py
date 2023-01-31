7c57646e9fc1cff15be2acbf4d65a451ec201b8b
AlenaSi <alenasinchura777@gmail.com>
2023-01-31 10:31:04 +0200

    first commit

---
+++ lesson_2.py
@@ -0,0 +1,15 @@
+print("Hello")
+print("s", 1, True)
+
+# age = input( "Please enter your age : ")
+age = "34"
+print( "Your age is :" , age)
+
+print("Type of variable age is :", type(age))
+
+age = int(age)
+print("Type of variable age is :", type (age))
+
+age_float = float(age)
+print("Type of variable age is :", type (age_float))
+
